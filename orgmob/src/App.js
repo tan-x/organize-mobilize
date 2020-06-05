57beaf4f77a26ccaabfba75dbb69456b0cdeb816
Tanner Griffin <tannermgriffin@gmail.com>
2020-06-05 14:32:34 -0500

    Created Category component and component folder for orginzation

--- orgmob/src/App.js
+++ orgmob/src/App.js
@@ -1,25 +1,18 @@
 import React from 'react';
 
-import Header from './Header'
+import Header from './components/Header'
+import Category from './components/Category'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <main>
+        <Category category="Volunteer" />
+        <Category category="Donate" />
+        <Category category="Petition" />
+      </main>
     </div>
   );
 }
